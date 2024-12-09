3195bea150e35b2c20a54b5684feb8acff7a4e28
ist-22 <113636662+ist-22@users.noreply.github.com>
2024-12-09 12:53:51 +0000

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace TestNRECrash
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMayCrashClicked(object sender, EventArgs e)
+        {
+            MayCrashBtn.Text += "";
+
+            // Comment below to avoid the crash that happens inside the try block
+            RunProcess();
+
+            try
+            {
+                object o = null;
+                MayCrashBtn.Text = o.ToString();
+            }
+            catch
+            {
+            }
+
+            MayCrashBtn.Text += "OK";
+        }
+
+        public static void RunProcess()
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "which",
+                    Arguments = "ls",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            process.StandardOutput.ReadToEnd();
+        }
+    }
+
+}
